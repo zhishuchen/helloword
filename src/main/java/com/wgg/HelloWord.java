2363d0898fbf5b160762225f46e12621169ec780
wgg <zhishuchen@qq.com>
2021-09-18 10:04:42 +0800

    第三次提交

--- src/main/java/com/wgg/HelloWord.java
+++ src/main/java/com/wgg/HelloWord.java
@@ -9,5 +9,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word!");
         System.out.println("Hello Word!2");
+        System.out.println("Hello Word!3");
     }
 }
