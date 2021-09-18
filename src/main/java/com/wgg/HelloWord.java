8e79dbea117f214b260b7d9b7b4ae97aa5e114d6
wgg <zhishuchen@qq.com>
2021-09-18 09:57:42 +0800

    第一次提交

---
+++ src/main/java/com/wgg/HelloWord.java
@@ -0,0 +1,12 @@
+package com.wgg;
+
+/**
+ * @author wgg
+ * @date 2021/9/15-9:48
+ * @describe
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
