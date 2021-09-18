8ddb38208e081c1ec02eeccf195d4a7624904fc8
wgg <zhishuchen@qq.com>
2021-09-18 10:03:50 +0800

    第二次提交

--- src/main/java/com/wgg/HelloWord.java
+++ src/main/java/com/wgg/HelloWord.java
@@ -8,5 +8,6 @@ package com.wgg;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word!");
+        System.out.println("Hello Word!2");
     }
 }
